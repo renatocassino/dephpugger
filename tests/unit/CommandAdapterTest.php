77e45c39e67b2d8421168903dadd373be7d7e83b
Tacnoman <renatocassino@gmail.com>
2017-02-25 18:14:50 -0300

    Change project name, because this name already exists

---
+++ tests/unit/CommandAdapterTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class CommandAdapterTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+
+    }
+}
--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,2 +1,13 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+require 'vendor/autoload.php';
+
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+
+$output = new ConsoleOutput();
+$output->setFormatter(new OutputFormatter(true));
+
+$output->writeln(
+    'The <comment>tests</comment> is <bg=magenta;fg=cyan;option=blink>running!</>'
+);
