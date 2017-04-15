b8257ab01d0ffd438bebbfaafa9cf550e65dce83
Tacnoman <renatocassino@gmail.com>
2017-04-15 14:39:56 -0300

    Testing dbgpcommand

---
+++ tests/unit/Command/DbgpCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Command;
+
+use Dephpug\Command\DbgpCommand;
+
+class DbgpCommandTest extends \Codeception\Test\Unit
+{
+    protected function _before()
+    {
+        $this->dbgpCommand = new DbgpCommand();
+    }
+
+    public function testMatchCommand()
+    {
+        $regexp = $this->dbgpCommand->getRegexp();
+        $matched = preg_match($regexp, 'dbgp(command)');
+        $this->assertTrue((bool) $matched);
+    }
+
+    public function testMatchCommandEmpty()
+    {
+        $regexp = $this->dbgpCommand->getRegexp();
+        $matched = preg_match($regexp, 'dbgp()');
+        $this->assertTrue(!$matched);        
+    }
+
+    public function testMatchCommandAndCheckValue()
+    {
+        $regexp = $this->dbgpCommand->getRegexp();
+        $matched = preg_match($regexp, 'dbgp(property_get -i 1)', $match);
+        $this->assertEquals('property_get -i 1', $match[1]);
+    }
+}
