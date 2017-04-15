3e69d8a950ad207922b4d1844736bab41e620aa9
Tacnoman <renatocassino@gmail.com>
2017-04-15 15:02:26 -0300

    Testing command list

---
+++ tests/unit/Command/ListCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Command;
+
+use Dephpug\Command\ListCommand;
+
+class ListCommandTest extends \Codeception\Test\Unit
+{
+    protected function _before()
+    {
+        $this->listCommand = new ListCommand();
+    }
+
+    public function testRegexpAliasL()
+    {
+        $regexp = $this->listCommand->getRegexp();
+        $matched = preg_match($regexp, 'l');
+        $this->assertTrue((bool) $matched);
+    }
+
+    public function testRegexpList()
+    {
+        $regexp = $this->listCommand->getRegexp();
+        $matched = preg_match($regexp, 'list');
+        $this->assertTrue((bool) $matched);
+    }
+}
