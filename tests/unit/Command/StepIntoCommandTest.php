254c10b628031696413d80a3b79d5f7688abb6f4
Tacnoman <renatocassino@gmail.com>
2017-04-16 14:56:05 -0300

    Step into command test

---
+++ tests/unit/Command/StepIntoCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Command;
+
+use Dephpug\Command\StepIntoCommand;
+
+class StepIntoCommandTest extends \Codeception\Test\Unit
+{
+    protected function _before()
+    {
+        $this->stepIntoCommand = new StepIntoCommand();
+    }
+
+    public function testRegexpAliasS()
+    {
+        $regexp = $this->stepIntoCommand->getRegexp();
+        $matched = preg_match($regexp, 's');
+        $this->assertTrue((bool) $matched);
+    }
+
+    public function testRegexpAliasStepInto()
+    {
+        $regexp = $this->stepIntoCommand->getRegexp();
+        $matched = preg_match($regexp, 'step_into');
+        $this->assertTrue((bool) $matched);
+    }
+}
