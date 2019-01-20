41662a8534ba27751011c60d4ee8cdee63835275
Tacnoman Cassino <renatocassino@gmail.com>
2019-01-20 18:29:09 -0200

    Command to install with phar file

---
+++ installer.sh
@@ -0,0 +1,5 @@
+curl https://raw.githubusercontent.com/tacnoman/dephpugger/master/dephpugger.phar -o dephpugger.phar
+chmod +x ./dephpugger.phar
+mv ./dephpugger.phar /usr/local/bin/dephpugger
+
+echo "\n\nDephpugger installed successfully!\nYou can use the Dephpugger command now 😎"
